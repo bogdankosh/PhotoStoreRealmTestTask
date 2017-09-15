c301cbfc909f8a93e4d80baa01d8f45e65ff5c18
Bohdan Koshyrets <bogdankosh@gmail.com>
2017-09-15 16:17:05 +0300

    Extension update

--- PhotoStoreRealmTestTask/Extensions.swift
+++ PhotoStoreRealmTestTask/Extensions.swift
@@ -39,6 +39,18 @@ extension UIViewController {
         alertController.addAction(OKAction)
         self.present(alertController, animated: true, completion: nil)
     }
-
 }
+
+extension FileManager {
+    class func documentsDir() -> String {
+        var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) as [String]
+        return paths[0]
+    }
+    
+    class func cachesDir() -> String {
+        var paths = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true) as [String]
+        return paths[0]
+    }
+}
+
 
